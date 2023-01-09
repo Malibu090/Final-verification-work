fb4b6d89679056f978ccf82611f4d827ff4cbd45
Malibu <malibu090@mail.ru>
2023-01-10 00:09:48 +0300

    Add program.cs

---
+++ Task/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] arr = { "dfgsgh", "dfg", "ljhnsad", "A", "3e2", "43kfgkl657", "gltehie45skf", "31" };
+int sizeArrayOutput = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i].Length <= 3)
+    {
+        sizeArrayOutput += 1;
+    }
+}
+
+void FillAndOutputArrayString(string[] arr)
+{
+    string[] arrayOutput = new string[sizeArrayOutput];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            arrayOutput[count] = arr[i];
+            count++;
+        }
+    }
+    System.Console.WriteLine("Массив строк, длина которых меньше или равна трём");
+    Console.WriteLine('[' + string.Join("; ", arrayOutput) + ']');
+}
+
+System.Console.WriteLine("Начальный массив строк");
+Console.WriteLine('[' + string.Join("; ", arr) + ']');
+FillAndOutputArrayString(arr);
